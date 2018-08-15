7e5d4c2f59094666e6c116c555d95929124c7ffb
zhangzaipeng <282354184@qq.com>
2018-08-15 20:41:46 +0800

    deploy sh

--- shex-deploy/auto_config/framework/bin/env.sh
+++ shex-deploy/auto_config/framework/bin/env.sh
@@ -5,6 +5,7 @@ APP_NAME=${appName}
 MODULE_NAME=${moduleName}
 
 TOMCAT_SERVER_HOME=${deployhome}/tomcat_server
-OUTPUT_HOME=${output}
-DEPLOY_HOME=${deployhome}
+OUTPUT_HOME=${output}/${APP_NAME}
+# /home/www/htdocs/shex-api-server/web-deploy
+DEPLOY_HOME=${deployhome}/${APP_NAME}/web-deploy
 
