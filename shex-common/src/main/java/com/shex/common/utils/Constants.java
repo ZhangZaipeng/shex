47255d37d386ef6cd060d524cb00e4bdf8dc598f
zhangzaipeng <282354184@qq.com>
2018-10-07 18:08:16 +0800

    init

--- shex-common/src/main/java/com/shex/common/utils/Constants.java
+++
@@ -1,8 +0,0 @@
-package com.shex.common.utils;
-
-/**
- * 静态常量常量
- */
-public class Constants {
-
-}
--- shex-common/src/main/java/com/shex/common/utils/i18n/I18nKeys.java
+++
@@ -1,6 +0,0 @@
-package com.shex.common.utils.i18n;
-
-public class I18nKeys {
-
-  public static String USER_LOGIN_ENTERTYPE = "bcex.service.user.login.enterType";
-}
